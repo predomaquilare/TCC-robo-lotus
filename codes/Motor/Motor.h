1189960b466e486efb4a09c3301c7e17870c01d8
puredo <104324993+predomaquilare@users.noreply.github.com>
2023-05-09 09:32:33 -0300

    Update Motor.h

--- codes/Motor/Motor.h
+++ codes/Motor/Motor.h
@@ -4,30 +4,17 @@
 
 class Motor {
   public:
-    Motor(byte *pubpins);
+    Motor(uint8_t *pubpins);
     void frente(int v = 4095);
     void esquerda(int v = 4095);
     void direita(int v = 4095);
     void PIDctrl(float pid, byte sensors);
   private:
-    byte valsensores;
+    uint8_t valsensores;
+    void noventagrausesq();
+    void noventagrausedir()
     unsigned long previousMillis = 0;
-    void noventagrausesq() {
-      unsigned long currentMillis = millis();
-      frente(4095 / 2);
-      while (millis() - previousMillis < 1000) {}
-      while ((valsensores & 0b00000110) == 0b00000110) {
-        esquerda();
-      }
-    }
-    void noventagrausdir() {
-      unsigned long currentMillis = millis();
-      frente(4095 / 2);
-      while (millis() - previousMillis < 1000) {}
-      while ((valsensores & 0b01100000) == 0b01100000) {
-        direita();
-      }
-    }
-    byte *pins;
+    uint8_t *pins;
 };
 #endif
+
